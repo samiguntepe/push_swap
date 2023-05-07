05ffa446ac033e3586e89ed67722b63bedbeb162
Sami Guntepe <samiguntepe@gmail.com>
2023-05-08 00:08:07 +0300

    42

--- include/libft/ft_strcmp.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int ft_strcmp(const char *s1, const char *s2) {
-    while (*s1 && (*s1 == *s2)) {
-        s1++;
-        s2++;
-    }
-    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
-}
